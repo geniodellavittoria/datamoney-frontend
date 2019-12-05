f793039e1955fb51d86ccf1cb5e71b37ad42e1ef
Pascal Keusch <pascal.keusch@gmail.com>
2019-12-05 14:55:13 +0100

    Add patient data component

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { AuthGuardService } from '../auth/auth.guard';
-import { RegisterComponent } from './register/register.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProfileComponent} from './profile/profile.component';
+import {AuthGuardService} from '../auth/auth.guard';
+import {RegisterComponent} from './register/register.component';
+import {AddPatientDataComponent} from './add-patient-data/add-patient-data.component';
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },
+  { path: 'patient', component: AddPatientDataComponent, canActivate: [AuthGuardService] },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: '**', redirectTo: 'register', pathMatch: 'full' },
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProfileComponent } from './profile/profile.component';
-import { RegisterComponent } from './register/register.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ProfileComponent} from './profile/profile.component';
+import {RegisterComponent} from './register/register.component';
 import {AuthModule} from '../auth/auth.module';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 import {UserRoutingModule} from './user-routing.module';
 import {MaterialModule} from '../material.module';
 import {SharedModule} from '../shared/shared.module';
-
+import {AddPatientDataComponent} from './add-patient-data/add-patient-data.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
     RegisterComponent,
+    AddPatientDataComponent,
   ],
   imports: [
     CommonModule,
